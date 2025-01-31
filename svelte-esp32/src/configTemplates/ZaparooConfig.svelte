ecde062efee4d89fb222a5c9500cc8ab0d56ee3d
v1605 <55302877+v1605@users.noreply.github.com>
2025-01-31 18:57:07 -0500

    ZapEnabled rename

--- svelte-esp32/src/configTemplates/ZaparooConfig.svelte
+++ svelte-esp32/src/configTemplates/ZaparooConfig.svelte
@@ -7,7 +7,7 @@
       event.preventDefault();
       const update: Partial<ConfigData> = {
         zapIp: config.zapIp, 
-        misterEnabled: config.misterEnabled,
+        zapEnabled: config.zapEnabled,
         steamIp: config.steamIp,
         steamEnabled: config.steamEnabled,
         resetOnRemove: config.resetOnRemove
@@ -23,17 +23,17 @@
     <div class="input-group">
       <div class="input-group-text">
         <div class="form-check form-switch col-2">
-          <input class="form-check-input" type="checkbox" data-bs-toggle="tooltip" title="Enable Zaparoo" data-bs-placement="top" role="switch" id="enablezaparoo" bind:checked={config.misterEnabled}/>
+          <input class="form-check-input" type="checkbox" data-bs-toggle="tooltip" title="Enable Zaparoo" data-bs-placement="top" role="switch" id="enablezaparoo" bind:checked={config.zapEnabled}/>
           <label class="form-check-label visually-hidden" for="enablezaparoo">Enable Zaparoo</label>
         </div>
       </div>
       <div class="form-floating">
-        <input type="text" class="form-control col-6" id="zaparooIp" placeholder="zaparoo.local" bind:value={config.zapIp} disabled={!config.misterEnabled} />
+        <input type="text" class="form-control col-6" id="zaparooIp" placeholder="zaparoo.local" bind:value={config.zapIp} disabled={!config.zapEnabled} />
         <label for="zaparooIp">Zaparoo Address</label>
       </div>
       <div class="input-group-text">
         <div class="form-check form-switch col-4">
-          <input class="form-check-input" type="checkbox" role="switch" id="enableRRoR" bind:checked={config.resetOnRemove} disabled={!config.misterEnabled}/>
+          <input class="form-check-input" type="checkbox" role="switch" id="enableRRoR" bind:checked={config.resetOnRemove} disabled={!config.zapEnabled}/>
           <label class="form-check-label" for="enableRRoR">Reset On Remove</label>
         </div>
       </div>
